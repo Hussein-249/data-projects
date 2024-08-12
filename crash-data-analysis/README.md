37b2ab9507278ad2a5801ecff786e9b4d5395c0e
Hussein-249 <husseinadi8@gmail.com>
2024-08-11 21:16:27 -0400

    Added basic distribution analysis.

--- crash-data-analysis/README.md
+++ crash-data-analysis/README.md
@@ -1,1 +1,7 @@
 # Crash Data Analysis Project
+
+## Abstract
+
+## Data Loading and Modelling
+
+The stucture of the data 
--- crash-data-analysis/data_analysis.R
+++ crash-data-analysis/data_analysis.R
@@ -1,6 +1,6 @@
-library(DBI)
-library(readr)
-library(ggplot2)
+library(DBI) # Database Interface
+library(readr) # Read Rectangular Text Data
+library(ggplot2) #Grammar of Graphics
 
 # ensures that the script's working directory aligns with the script's location
 # change this if your data is stored separately from your script
@@ -11,9 +11,11 @@ rm(path)
 getwd()
 dir()
 
+### Part 1 - Data Load ###
+
 csv_filename <- "crashdata.csv"
 
-# set to FALSE if your data is stored in a Postgres database
+# set to FALSE if your data is stored in a Postgres database, then replace auth
 use_csv <- TRUE
 
 if (use_csv) {
@@ -53,11 +55,40 @@ random_indices <- sample(nrow(df), 500)
 # Subset the dataframe using the random indices
 random_df <- df[random_indices, ]
 
-numeric_df = df[sapply(df, is.numeric)]
+head(random_df)
 
-head(df)
+# ignore for now!
 
-unique(df$collisiontype)
+# numeric_df <- df[sapply(df, is.numeric)]
 
-# in progress
-# glm(injuries ~ ., data = random_df, family = "binomial")
+# unique(df$injuries)
+
+### Part 2 - Analyzing Distributions ###
+
+with(random_df, {
+  
+  pdf("Injuries-QQ-Plot.pdf", title = "Injuries QQ Plot Title")
+  
+  true_injuries <- vehicleaestspeed_kmh[injuries == TRUE]
+  false_injuries <- vehicleaestspeed_kmh[injuries == FALSE]
+  
+  par(mfrow = c(2, 1))
+  qqnorm(false_injuries, main = "Q-Q No Injuries")
+  qqline(false_injuries, col = "green")
+  
+  qqnorm(true_injuries, main = "Q-Q Injuries")
+  qqline(true_injuries, col = "red")
+  
+  dev.off()
+  
+  true_shapiro_test <- shapiro.test(true_injuries)
+  print(true_shapiro_test)
+  
+  true_shapiro_test <- shapiro.test(false_injuries)
+  print(true_shapiro_test)
+  
+  summary(true_injuries)
+  summary(false_injuries)
+})
+
+### Part 3 - ANOVA ###
