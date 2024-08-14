5d028a39d0cf74d8a09f4fa75c60f6037f6e572b
Hussein-249 <husseinadi8@gmail.com>
2024-08-13 21:33:13 -0400

    Added basic t-test.

--- crash-data-analysis/data_analysis.R
+++ crash-data-analysis/data_analysis.R
@@ -1,6 +1,6 @@
 library(DBI) # Database Interface
 library(readr) # Read Rectangular Text Data
-library(ggplot2) #Grammar of Graphics
+library(ggplot2) # Grammar of Graphics
 
 # ensures that the script's working directory aligns with the script's location
 # change this if your data is stored separately from your script
@@ -8,10 +8,11 @@ path <- dirname(rstudioapi::getActiveDocumentContext()$path)
 setwd(path)
 rm(path)
 
+# check to ensure that the wd is correct
 getwd()
 dir()
 
-### Part 1 - Data Load ###
+###   Part 1 - Data Load and Model ###
 
 csv_filename <- "crashdata.csv"
 
@@ -50,36 +51,32 @@ if (use_csv) {
 
 df <- subset(df, select = -incidentid)
 
-random_indices <- sample(nrow(df), 500)
+random_indices <- sample(nrow(df), 750)
 
 # Subset the dataframe using the random indices
 random_df <- df[random_indices, ]
 
 head(random_df)
 
-# ignore for now!
-
-# numeric_df <- df[sapply(df, is.numeric)]
-
 # unique(df$injuries)
 
-### Part 2 - Analyzing Distributions ###
+###   Part 2 - Analyzing Distributions ###
 
 with(random_df, {
   
-  pdf("Injuries-QQ-Plot.pdf", title = "Injuries QQ Plot Title")
+  # pdf("Injuries-QQ-Plot.pdf", title = "Injuries QQ Plot Title")
   
   true_injuries <- vehicleaestspeed_kmh[injuries == TRUE]
   false_injuries <- vehicleaestspeed_kmh[injuries == FALSE]
   
-  par(mfrow = c(2, 1))
+  par(mfrow = c(1, 2))
   qqnorm(false_injuries, main = "Q-Q No Injuries")
   qqline(false_injuries, col = "green")
   
   qqnorm(true_injuries, main = "Q-Q Injuries")
   qqline(true_injuries, col = "red")
   
-  dev.off()
+  # dev.off()
   
   true_shapiro_test <- shapiro.test(true_injuries)
   print(true_shapiro_test)
@@ -89,6 +86,46 @@ with(random_df, {
   
   summary(true_injuries)
   summary(false_injuries)
+  
+  # reset the output to single plot per image
+  par(mfrow = c(1, 1))
+  
+  boxplot(true_injuries, false_injuries, main = "Boxplot of speed based on injuries")
+  
+  t_test_result <- t.test(true_injuries, false_injuries)
+  
+  print(t_test_result)
 })
 
-### Part 3 - ANOVA ###
+###   Part 3 - Chi Squared Test ### 
+
+with(random_df, { 
+  
+})
+
+###   Part 4 - PCA  ### 
+
+with(random_df, { 
+  
+  df <- subset(df, select = c(vehicleadriverage,
+                              vehiclebdriverage,
+                              surfacecoefficient_percent,
+                              vehicleaestspeed_kmh,
+                              vehiclebestspeed_kmh,
+                              vehicleabrakedist_m))
+  
+  random_indices <- sample(nrow(df), 50)
+  
+  # Subset the dataframe using the random indices
+  random_df <- df[random_indices, ]
+  
+  numeric_df <- random_df[sapply(random_df, is.numeric)]
+  
+  scaled_df <- scale(numeric_df)
+  
+  PCA_result <- prcomp(scaled_df, center = TRUE, scale. = TRUE)
+  
+  biplot(PCA_result)
+  summary(PCA_result) 
+
+  })
